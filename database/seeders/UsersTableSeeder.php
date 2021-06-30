13bc7355de25f4228e66fa0f2a3c35560e609242
alexandrepalmag <alepalmag85ti@gmail.com>
2021-06-30 01:05:11 -0300

    Factories functions to create users.

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
+use Carbon\Factory;
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -17,7 +19,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(
+        /* DB::table('users')->insert(
             [
                 'name' => 'Administrator',
                 'email' => 'admin@gmail.com',
@@ -25,6 +27,10 @@ class UsersTableSeeder extends Seeder
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => 'abababab',
             ]
-        );
+        ); */
+
+
+      /*  User::factory()->count(50)->create(); */
+        
     }
 }
