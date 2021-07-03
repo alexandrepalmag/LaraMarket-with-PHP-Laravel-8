760af7ab3fd515c7dbfbb201d0f2b6ddfcd07179
alexandrepalmag <alepalmag85ti@gmail.com>
2021-07-03 13:21:01 -0300

    creation create and store methods on StoreCOntroller

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Store;
+use App\Models\User;
 
 class StoreController extends Controller
 {
@@ -14,5 +15,16 @@ class StoreController extends Controller
         $stores = Store::paginate(10);
 
         return view('admin.stores.index', compact('stores'));
+    }
+
+    public function create()
+    {
+        $users = User::all(['id', 'name']);
+        return view('admin.stores.create', compact('users'));
+    }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
     }
 }
